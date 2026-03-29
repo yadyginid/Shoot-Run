f02b6754e8ba5c5a35b8dec59ee10dcd66e7a492
MatrX <47641699+qmatrxp@users.noreply.github.com>
2026-03-29 13:35:34 +0300

    Обновлено до 5.7
    Добавлены контроллеры для андройда
    Добавлен PlayerController и частично перенесено управление

--- Source/ShootAndRun.Target.cs
+++ Source/ShootAndRun.Target.cs
@@ -8,7 +8,7 @@ public class ShootAndRunTarget : TargetRules
 	public ShootAndRunTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("ShootAndRun");
 	}
--- Source/ShootAndRunEditor.Target.cs
+++ Source/ShootAndRunEditor.Target.cs
@@ -8,7 +8,7 @@ public class ShootAndRunEditorTarget : TargetRules
 	public ShootAndRunEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("ShootAndRun");
 	}
